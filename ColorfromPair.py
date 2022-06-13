e046db593ee4fe6be11994c89a9011d11027b060
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-13 18:23:39 +0530

    Update ColorfromPair.py

--- ColorfromPair.py
+++ ColorfromPair.py
@@ -3,7 +3,7 @@ import definitions
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
   major_index = zero_based_pair_number // len(definitions.MINOR_COLORS)
-  if major_index >= len(definitions.definitions.MAJOR_COLORS):
+  if major_index >= len(definitions.MAJOR_COLORS):
     raise Exception('Major index out of range')
   minor_index = zero_based_pair_number % len(definitions.MINOR_COLORS)
   if minor_index >= len(definitions.MINOR_COLORS):
