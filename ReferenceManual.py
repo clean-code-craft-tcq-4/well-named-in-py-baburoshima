88d8d3817f3c4029d281463c1d8b081aa4dfcfa3
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-13 18:22:28 +0530

    Update ReferenceManual.py
    adding print manual function

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -1,4 +1,16 @@
-import PairfromColor , ColorfromPair , definitions
 
-def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
+import PairfromColor, ColorfromPair ,definitions
+
+def print_manual(major_color, minor_color):
+  print ('REFERENCE MANUAL for 25-pair color code' + '\n'  + '---------------------------------------')
+  print ('PAIR NUMBER : MAJOR COLOR : MINOR COLOR')
+
+  for major_color in definitions.MAJOR_COLORS:
+    for minor_color in definitions.MINOR_COLORS:
+      pair_number = PairfromColor.get_pair_number_from_color(major_color,minor_color )
+      major_color, minor_color = ColorfromPair.get_color_from_pair_number(pair_number)
+      print(color_pair_to_string(pair_number, major_color, minor_color))
+
+def color_pair_to_string(pair_number, major_color, minor_color):
+  return f' {pair_number} : {major_color} : {minor_color}  '
+
