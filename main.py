51b63957f56bb1ff6d7e5c6cad799380f854093c
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-13 18:24:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import testfile
+import testfile,definitions
 import ReferenceManual
-
 
 if __name__ == '__main__':
   testfile.test_number_to_pair(4, 'White', 'Brown')
@@ -9,3 +8,4 @@ if __name__ == '__main__':
   testfile.test_pair_to_number('Violet', 'Slate', 25)
   testfile.test_pair_to_number('Red', 'Orange', 7)
   print('Done :)')
+  ReferenceManual.print_manual(definitions.MAJOR_COLORS, definitions.MINOR_COLORS)
