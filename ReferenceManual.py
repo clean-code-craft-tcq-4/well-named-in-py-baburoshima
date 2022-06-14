8d1820e60ead575489412b247f2121ec30b2c063
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-14 21:54:29 +0530

    Update ReferenceManual.py
    Formatting reference manual print

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -1,9 +1,9 @@
-
 import PairfromColor, ColorfromPair ,definitions
 
+# New function to print reference manual
 def print_manual(major_color, minor_color):
   print ('REFERENCE MANUAL for 25-pair color code' + '\n'  + '---------------------------------------')
-  print ('PAIR NUMBER : MAJOR COLOR : MINOR COLOR')
+  print ('PAIR NUMBER | MAJOR COLOR | MINOR COLOR' + '\n'  + '---------------------------------------')
 
   for major_color in definitions.MAJOR_COLORS:
     for minor_color in definitions.MINOR_COLORS:
@@ -12,5 +12,5 @@ def print_manual(major_color, minor_color):
       print(color_pair_to_string(pair_number, major_color, minor_color))
 
 def color_pair_to_string(pair_number, major_color, minor_color):
-  return f' {pair_number} : {major_color} : {minor_color}  '
+  return f'| {pair_number:9} | {major_color:11} | {minor_color:10} |'
 
